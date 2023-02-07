bb9d77fa8539f4aa54d01fd492276ce56efa5c47
TheMattWang <maw2025@tamu.edu>
2023-02-07 12:13:45 -0600

    creation of style 2 and editing of styles1

--- functions.js
+++ functions.js
@@ -1,9 +1,30 @@
+function load_style() {
+    page_style = localStorage.getItem("page_stylesheet_name");
+    if(page_style === null){
+        page_style = "styles1.css";
+    }
+    document.getElementById('page_style').setAttribute("href",page_style);
+}
+
+
 const button = document.getElementById("changeCSS");
+const link = document.getElementById("currentCSS");
+button.addEventListener("click", alert("button was clicked"));
 button.addEventListener("click", function() {
-  const currentLink = document.querySelector("link[rel='stylesheet']");
-  if (currentLink.getAttribute("href") === "style1.css") {
-    currentLink.setAttribute("href", "style2.css");
-  } else {
-    currentLink.setAttribute("href", "style1.css");
-  }
-});
+    if(link.getAttribute("href") === "styles1.css") {
+        link.setAttribute("href","styles2.css");
+        localStorage.setItem("css", "styles2.css");
+    } else {
+        link.setAttribute("href","styles1.css");
+        localStorage.setItem("css", "styles2.css");
+    }
+}
+
+);
+const storedCSS = localStorage.getItem("css");
+if(storedCSS) {
+    link.setAttribute("href",storedCSS);
+}
+
+
+
